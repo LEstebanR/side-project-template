01c02253572b2fa4139dea9d88a663f0aabfaf30
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-08-13 18:29:16 -0500

    initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,17 @@ import { Chip } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-4 md:p-24 bg-light dark:bg-dark ">
-      <div className="p-2 w-full rounded-xl flex flex-col items-center justify-center py-8 shadow-secondary shadow-complete gap-6">
-        <Title gradient>Side Projects</Title>
-        <Body>This is a template to create Next.js apps.</Body>
-        <Subtitle color="secondary">Colors:</Subtitle>
-        <div className="flex gap-2 flex-wrap justify-center">
-          <Chip color="primary">Primary</Chip>
-          <Chip color="secondary">Secondary</Chip>
-          <Chip color="success">Success</Chip>
-          <Chip color="warning">Warning</Chip>
-          <Chip color="danger">Danger</Chip>
-        </div>
+    <div className="px-2 rounded-xl flex flex-col items-center justify-center py-8 shadow-secondary shadow-complete gap-6 lg:w-8/12">
+      <Title gradient>Side Projects</Title>
+      <Body>This is a template to create Next.js apps.</Body>
+      <Subtitle color="secondary">Colors:</Subtitle>
+      <div className="flex gap-2 flex-wrap justify-center">
+        <Chip color="primary">Primary</Chip>
+        <Chip color="secondary">Secondary</Chip>
+        <Chip color="success">Success</Chip>
+        <Chip color="warning">Warning</Chip>
+        <Chip color="danger">Danger</Chip>
       </div>
-    </main>
+    </div>
   );
 }
